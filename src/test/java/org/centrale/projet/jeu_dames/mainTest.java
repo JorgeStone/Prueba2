6e9bd2d6104f36405a6592999d0a53d7a80e7529
JorgeStone <jqcc17@gmail.com>
2021-12-27 19:22:04 +0100

    version1

---
+++ src/test/java/org/centrale/projet/jeu_dames/mainTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.projet.jeu_dames;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author jquis
+ */
+public class mainTest {
+    
+    public mainTest() {
+    }
+
+//    @org.junit.jupiter.api.BeforeAll
+//    public static void setUpClass() throws Exception {
+//    }
+//
+//    @org.junit.jupiter.api.AfterAll
+//    public static void tearDownClass() throws Exception {
+//    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of main method, of class main.
+     */
+    @org.junit.jupiter.api.Test
+    public void testEquals() {
+        tablier my_tablier= new tablier();
+        my_tablier.creerJeu();
+        assertEquals(0, my_tablier.getMatrice()[0][0]);
+    }
+}
