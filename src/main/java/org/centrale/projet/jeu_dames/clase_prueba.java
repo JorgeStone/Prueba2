9dc274c0c2ca5e9341d34c9984e0c1ee29ee944b
JorgeStone <jqcc17@gmail.com>
2021-12-27 19:51:41 +0100

    version2

--- src/main/java/org/centrale/projet/jeu_dames/clase_prueba.java
+++ src/main/java/org/centrale/projet/jeu_dames/clase_prueba.java
@@ -2,6 +2,7 @@ package org.centrale.projet.jeu_dames;
 
 public class clase_prueba {
     public static void main(String args[]){
-        System.out.println("Holaaa");
+        int i=0;
+        System.out.println("buenas");
     }
 }
