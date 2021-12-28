820e930bca889cf79013239c743c67c6d1816ba8
JorgeStone <jqcc17@gmail.com>
2021-12-28 18:12:14 +0100

    añado nuevo archico

--- src/main/java/org/centrale/projet/jeu_dames/nuevafuncionalidad.java
+++ src/main/java/org/centrale/projet/jeu_dames/nuevafuncionalidad.java
@@ -1,8 +1,8 @@
 package org.centrale.projet.jeu_dames;
 
-public class clase_prueba2 {
+public class nuevafuncionalidad {
     public static void main(String args[]){
         int i=0;
-        System.out.println("buen");
+        System.out.println("m1");
     }
 }
